7cbae50778ef9c830e884ea11b6b9d64f9336c62
AzaharSheikh <azaharsheikh786@gmail.com>
2016-09-28 16:54:08 +0530

    login file added

--- app/src/main/java/com/thoughtinterac/realestateapp/Activities/LoginActivity.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Activities/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.thoughtinterac.realestateapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.thoughtinterac.realestateapp.R;
 
@@ -10,9 +13,18 @@ import com.thoughtinterac.realestateapp.R;
  * Created by AzaharSheikh on 28-09-2016.
  */
 public class LoginActivity extends AppCompatActivity {
+    Button btn_login;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        btn_login=(Button)findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
