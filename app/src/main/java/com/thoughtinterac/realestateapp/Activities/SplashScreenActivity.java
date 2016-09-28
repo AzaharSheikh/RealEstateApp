1ebdf390bcfc815c7d3865eea2fee02d15142c35
AzaharSheikh <azaharsheikh786@gmail.com>
2016-09-28 17:18:12 +0530

    login file added

--- app/src/main/java/com/thoughtinterac/realestateapp/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Activities/SplashScreenActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.thoughtinterac.realestateapp.R;
 
@@ -11,22 +14,28 @@ import com.thoughtinterac.realestateapp.R;
  * Created by AzaharSheikh on 28-09-2016.
  */
 public class SplashScreenActivity extends AppCompatActivity {
+    TextView textView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
-                    sleep(3000);
-                }catch(InterruptedException e){
+
+        textView = (TextView) findViewById(R.id.text);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+                    textView.startAnimation(animation);
+                    sleep(5000);
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
-                    startActivity(intent);
+                } finally {
+                    Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
                 }
             }
         };
-        timerThread.start();
+        timer.start();
     }
 }
