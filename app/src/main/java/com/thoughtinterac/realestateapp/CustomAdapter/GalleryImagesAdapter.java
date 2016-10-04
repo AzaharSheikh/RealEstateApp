88cfb0ad27f450bf52bb40fd49ab5dd8e2192030
AzaharSheikh <azaharsheikh786@gmail.com>
2016-10-04 12:09:15 +0530

    my project update

--- app/src/main/java/com/thoughtinterac/realestateapp/CustomAdapter/GalleryImagesAdapter.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/CustomAdapter/GalleryImagesAdapter.java
@@ -3,7 +3,12 @@ package com.thoughtinterac.realestateapp.CustomAdapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,27 +64,7 @@ public class GalleryImagesAdapter extends BaseAdapter {
         ImageView img_single_image=(ImageView)convertView.findViewById(R.id.img_single_image);
         LinearLayout li_main=(LinearLayout)convertView.findViewById(R.id.li_main);
         final GalleryImagesModel m = imageItems.get(position);
-        /*Button btn_site_visit=(Button)convertView.findViewById(R.id.btn_site_visit);
-        // getting movie data for the row
-        final Product m = productItems.get(position);
-        btn_site_visit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                Toast.makeText(activity,m.getTitle(),Toast.LENGTH_LONG).show();
 
-                Intent i = new Intent(activity, Product_Details_Activity.class);
-                bundle.putString("title", m.getTitle());
-                bundle.putString("details", m.getP_details());
-                bundle.putString("address", m.getP_address());
-                i.putExtras(bundle);
-                activity.startActivity(i);
-            }
-        });*/
-        // thumbnail image
-
-
-        // title
         li_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,6 +76,20 @@ public class GalleryImagesAdapter extends BaseAdapter {
                 bundle.putString("img_name", m.getImg_name());
                 i.putExtras(bundle);
                 activity.startActivity(i);
+            }
+        });
+        li_main.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Drawable myDrawable = activity.getResources().getDrawable(m.getImage_id());
+                Bitmap anImage      = ((BitmapDrawable) myDrawable).getBitmap();
+                String pathofBmp = MediaStore.Images.Media.insertImage(activity.getContentResolver(), anImage,m.getImg_name(), null);
+                Uri bmpUri = Uri.parse(pathofBmp);
+                final Intent emailIntent1 = new Intent(     android.content.Intent.ACTION_SEND);
+                emailIntent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                emailIntent1.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                emailIntent1.setType("image/png");
+                return true;
             }
         });
         img_single_image.setImageResource(m.getImage_id());
