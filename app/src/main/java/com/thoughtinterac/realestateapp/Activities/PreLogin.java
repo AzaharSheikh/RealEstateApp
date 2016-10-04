2497003cf756dc08cf6f443a11e20cde34d351a6
AzaharSheikh <azaharsheikh786@gmail.com>
2016-10-04 15:35:47 +0530

    my project update

--- app/src/main/java/com/thoughtinterac/realestateapp/Activities/PreLogin.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Activities/PreLogin.java
@@ -1,8 +1,11 @@
 package com.thoughtinterac.realestateapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.thoughtinterac.realestateapp.R;
 
@@ -11,9 +14,19 @@ import com.thoughtinterac.realestateapp.R;
  */
 public class PreLogin extends AppCompatActivity
 {
+    ImageView img_user;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pre_login);
+        img_user=(ImageView)findViewById(R.id.img_user);
+        img_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PreLogin.this,LoginActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
