30004d9c4383c1e556f6eb0c179ec7fda7545636
AshwiniBadgujar <ashwinibadgujar3@gmail.com>
2016-10-07 14:58:44 +0530

    t and c

--- app/src/main/java/com/thoughtinterac/realestateapp/Fragments/PrivacyPolicyFragment.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Fragments/PrivacyPolicyFragment.java
@@ -19,7 +19,7 @@ public class PrivacyPolicyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View rootView = inflater.inflate(R.layout.mybankdetails, container, false);
+        final View rootView = inflater.inflate(R.layout.privacypolicy, container, false);
         return rootView;
     }
 }
