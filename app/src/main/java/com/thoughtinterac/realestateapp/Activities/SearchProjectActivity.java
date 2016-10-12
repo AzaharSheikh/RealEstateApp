d2b099f3f3c20a42b8ce8cc99f999664e570449e
AzaharSheikh <azaharsheikh786@gmail.com>
2016-10-12 12:28:58 +0530

    added user list realtor

---
+++ app/src/main/java/com/thoughtinterac/realestateapp/Activities/SearchProjectActivity.java
@@ -0,0 +1,15 @@
+package com.thoughtinterac.realestateapp.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AzaharSheikh on 12-10-2016.
+ */
+public class SearchProjectActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
