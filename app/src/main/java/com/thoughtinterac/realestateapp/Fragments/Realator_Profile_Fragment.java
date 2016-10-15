e9dbe0348a517451a4c3419214710bea29f3ba4c
AzaharSheikh <azaharsheikh786@gmail.com>
2016-10-15 18:47:55 +0530

    welcome new layout added

--- app/src/main/java/com/thoughtinterac/realestateapp/Fragments/Realator_Profile_Fragment.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Fragments/Realator_Profile_Fragment.java
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.thoughtinterac.realestateapp.Activities.MainActivity;
+import com.thoughtinterac.realestateapp.Activities.RealtorMainPage;
 import com.thoughtinterac.realestateapp.R;
 
 import java.sql.DatabaseMetaData;
@@ -14,7 +17,7 @@ import java.sql.DatabaseMetaData;
  * Created by AshwiniBadgujar on 14-10-2016.
  */
 public class Realator_Profile_Fragment extends Fragment {
-
+    TextView name_text;
     public Realator_Profile_Fragment() {
     }
 
@@ -23,6 +26,8 @@ public class Realator_Profile_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.welcome_realator, container, false);
+        name_text=(TextView)rootView.findViewById(R.id.name_text);
+        name_text.setText(RealtorMainPage.str_user_name);
         return rootView;
 
     }
