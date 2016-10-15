7388a9fbdfda618e92e650101a9df4d507f692ef
AzaharSheikh <azaharsheikh786@gmail.com>
2016-10-15 16:25:03 +0530

    navigation header name and email added

--- app/src/main/java/com/thoughtinterac/realestateapp/Fragments/User_Welcome_Page.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Fragments/User_Welcome_Page.java
@@ -6,13 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.thoughtinterac.realestateapp.Activities.MainActivity;
 import com.thoughtinterac.realestateapp.R;
 
 /**
  * Created by AshwiniBadgujar on 14-10-2016.
  */
 public class User_Welcome_Page extends Fragment {
+    TextView name_text;
     public User_Welcome_Page() {
     }
 
@@ -21,6 +24,8 @@ public class User_Welcome_Page extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.welcome_user, container, false);
+        name_text=(TextView)rootView.findViewById(R.id.name_text);
+        name_text.setText(MainActivity.str_user_name);
         return rootView;
     }
 }
