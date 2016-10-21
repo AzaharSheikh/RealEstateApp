cabdf6b9ea3429c43bfebfff3abd8ac3a1a3f04f
AshwiniBadgujar <ashwinibadgujar3@gmail.com>
2016-10-21 17:34:54 +0530

    forget password userlist welcome name

--- app/src/main/java/com/thoughtinterac/realestateapp/Activities/UserProfileUpdateActivity.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Activities/UserProfileUpdateActivity.java
@@ -83,12 +83,27 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
        if(bundle!=null) {
            String name = bundle.getString(DatabaseHandler.KEY_USER_NAME);
            edt_username.setText(name);
+
            String str_user_address  = bundle.getString(DatabaseHandler.KEY_USER_ADDRESS);
            edt_user_address.setText(str_user_address);
+
            String str_job_dec  = bundle.getString(DatabaseHandler.KEY_USER_JOB_DESC);
-           edt_mobile.setText(str_job_dec);
+           edt_job_dec.setText(str_job_dec);
+
+           String str_mobile  = bundle.getString(DatabaseHandler.KEY_USER_MOBILE);
+           edt_mobile.setText(str_mobile);
+
            String str__email  = bundle.getString(DatabaseHandler.KEY_USER_EMAIL);
            edt_email.setText(str__email);
+
+
+           String str_pan  = bundle.getString(DatabaseHandler.KEY_PAN_NUMBER);
+           edt_pan.setText(str_pan);
+
+           String str_bank  = bundle.getString(DatabaseHandler.KEY_BANK_DETAILS);
+           edt_bank_details.setText(str__email);
+
+
        }
 
     }
