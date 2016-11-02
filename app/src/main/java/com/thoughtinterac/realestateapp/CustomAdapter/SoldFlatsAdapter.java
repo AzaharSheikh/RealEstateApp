85a2f6ac5e582590891843408bc73ed2bf299cb2
AzaharSheikh <azaharsheikh786@gmail.com>
2016-11-02 10:38:01 +0530

    sold flats list added

---
+++ app/src/main/java/com/thoughtinterac/realestateapp/CustomAdapter/SoldFlatsAdapter.java
@@ -0,0 +1,7 @@
+package com.thoughtinterac.realestateapp.CustomAdapter;
+
+/**
+ * Created by AzaharSheikh on 02-11-2016.
+ */
+public class SoldFlatsAdapter {
+}
---
+++ app/src/main/java/com/thoughtinterac/realestateapp/Model/SoldFlatsModel.java
@@ -0,0 +1,7 @@
+package com.thoughtinterac.realestateapp.Model;
+
+/**
+ * Created by AzaharSheikh on 02-11-2016.
+ */
+public class SoldFlatsModel {
+}
