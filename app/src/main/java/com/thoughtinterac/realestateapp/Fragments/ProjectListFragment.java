71afef24833fa70241cec60f38d3d8209de384ba
AzaharSheikh <azaharsheikh786@gmail.com>
2016-11-09 10:12:46 +0530

    gemin splendor text add

--- app/src/main/java/com/thoughtinterac/realestateapp/Fragments/ProjectListFragment.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Fragments/ProjectListFragment.java
@@ -33,12 +33,12 @@ public class ProjectListFragment extends Fragment {
         projectDate = new ArrayList<>();
         projectType = new ArrayList<>();
 
-        projectName.add("Gemini");
+        projectName.add("Gemini Splendor");
         projectLocation.add("Dubai");
         projectDate.add("20/04/2016");
         projectType.add("1BHK");
 
-        projectName.add("Gemini");
+        projectName.add("Gemini Splendor");
         projectLocation.add("Dubai");
         projectDate.add("22/04/2016");
         projectType.add("2BHK");
