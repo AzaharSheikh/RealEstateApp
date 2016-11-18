02f9b30f2a3e71a2138a542a921d721628d335b4
AzaharSheikh <azaharsheikh786@gmail.com>
2016-11-18 20:09:29 +0530

    final changes done 90%

--- app/src/main/java/com/thoughtinterac/realestateapp/Fragments/User_Welcome_Page.java
+++ app/src/main/java/com/thoughtinterac/realestateapp/Fragments/User_Welcome_Page.java
@@ -1,12 +1,16 @@
 package com.thoughtinterac.realestateapp.Fragments;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,6 +27,7 @@ import com.thoughtinterac.realestateapp.R;
  */
 public class User_Welcome_Page extends Fragment {
     TextView name_text;
+    WebView webView_welcome_user_slider;
     public User_Welcome_Page() {
     }
 
@@ -40,6 +45,18 @@ public class User_Welcome_Page extends Fragment {
         ImageView img_user_installment_paid = (ImageView)rootView.findViewById(R.id.img_user_installment_paid);
         ImageView img_myproject = (ImageView)rootView.findViewById(R.id.img_myproject);
         ImageView img_mydocuments = (ImageView)rootView.findViewById(R.id.img_mydocuments);
+        webView_welcome_user_slider=(WebView)rootView.findViewById(R.id.webView_welcome_user_slider);
+        webView_welcome_user_slider.getSettings().setJavaScriptEnabled(true);
+        webView_welcome_user_slider.getSettings().setLoadWithOverviewMode(true);
+        webView_welcome_user_slider.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView_welcome_user_slider.getSettings().setUseWideViewPort(true);
+        webView_welcome_user_slider.getSettings().setBuiltInZoomControls(true);
+        webView_welcome_user_slider.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView_welcome_user_slider.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView_welcome_user_slider.setBackgroundColor(Color.TRANSPARENT);
+       // webView_welcome_user_sliderme.getSettings().setPluginsEnabled(true);
+        webView_welcome_user_slider.setWebViewClient(new HelloWebViewClient());
+        webView_welcome_user_slider.loadUrl("file:///android_asset/www/welcome_user_slider/index.html");
 
         img_project_status.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,5 +87,11 @@ public class User_Welcome_Page extends Fragment {
             }
         });
         return rootView;
+    }
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
     }
 }
